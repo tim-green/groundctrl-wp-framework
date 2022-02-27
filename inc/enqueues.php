f951a4e16ee3089ebc9419076b7c211ddf1f2c7e
Tim <github@timgreen.ws>
2022-02-28 09:51:46 +1100

    added theme version

--- inc/enqueues.php
+++ inc/enqueues.php
@@ -9,6 +9,8 @@ if ( ! function_exists( 'grnd_styles_scripts' ) ) {
 
 	function grnd_styles_scripts() {
 
+		$theme_version = wp_get_theme()->get( 'Version' );
+
 		// CSS
 		wp_enqueue_style( 'grnd-styles', get_template_directory_uri() . '/assets/build/app.min.css', false, $theme_version, 'all' );
 
