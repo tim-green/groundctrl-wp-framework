9ca2783e1f3df185a922a8d525a732427cc2e8fa
Tim <github@timgreen.ws>
2022-02-28 09:50:10 +1100

    added function for google fonts preconnect

--- inc/enqueues.php
+++ inc/enqueues.php
@@ -4,3 +4,10 @@
  * GroundCTRL Framework 
  * https://developer.wordpress.org/themes/basics/including-css-javascript/
  */
+
+// Disable this action if not loading Google Fonts from their external server
+// function grnd_google_fonts_preconnect() {
+// 	echo '<link rel="preconnect" href="https://fonts.googleapis.com">' . "\n";
+// 	echo '<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>' . "\n";
+// }
+// add_action( 'wp_head', 'grnd_google_fonts_preconnect', 7 );
